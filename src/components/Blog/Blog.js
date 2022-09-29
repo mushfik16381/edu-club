df86ee4e5fa2b9366dfef9e95b8e7cf99daa1c65
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-09-29 21:47:27 +0600

    qus added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -13,52 +13,41 @@ const Blog = () => {
                   <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
                       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        What is difference between VAR, let and CONST?
+                        How does react work?
 
                       </button>
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                       <div class="accordion-body">
-                        <p>The scope of a var variable is functional scope. The scope of a let variable is block scope. The scope of a const variable is block scope. It can be updated and re-declared into the scope.Var variable is an old method to declare a variable in javascript. In modern javascript, we use the let and const variable.</p>
+                        <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
                       </div>
                     </div>
                   </div>
                   <div class="accordion-item">
                     <h2 class="accordion-header" id="headingTwo">
                       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        What is the difference between arrow and normal function?
+                      What is difference between state and props in React?
                       </button>
                     </h2>
                     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                       <div class="accordion-body">
-                        <p>Regular functions created using function declarations or expressions are constructible and callable. Since regular functions are constructible, they can be called using the new keyword. However, the arrow functions are only callable and not constructible, i.e arrow functions can never be used as constructor functions.</p>
+                        <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
                       </div>
                     </div>
                   </div>
                   <div class="accordion-item">
                     <h2 class="accordion-header" id="headingThree">
                       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        What is difference between map and forEach and filter?
+                      What are the use cases of use effect hook?
                       </button>
                     </h2>
                     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                       <div class="accordion-body">
-                        <p>Foreach takes a callback function and run that callback function on each element of array one by one.</p>
-                        <p>Function .filter() is a search function that returns all the elements that fulfil the assigned condition.</p>
-                        <p>Map ran through every element of the array, multiplied it to 10 and returned the element which will be going to store inside our resulting array.</p>
-                        <p>Function .find() is also a search function like the previous but they differ in one small detail — this function returns only one match in an array. </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingFour">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                        What is template string?
-                      </button>
-                    </h2>
-                    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                      <div class="accordion-body">
-                        <p>Template strings are a powerful feature of modern JavaScript released in ES6. It lets us insert/interpolate variables and expressions into strings without needing to concatenate like in older versions of JavaScript. It allows us to create strings that are complex and contain dynamic elements.</p>
+                        <li>Running once on mount: fetch API data.</li>
+                        <li>Running on state change: validating input field.</li>
+                        <li>Running on state change: live filtering.</li>
+                        <li>Running on state change: trigger animation on new array value.</li>
+                        <li>Running on props change: update paragraph list on fetched API data update.</li>
                       </div>
                     </div>
                   </div>
